65c739f03e349e0ad1ec25ab2dde308efe549570
Wilhem Arnoldy <contact@wilhemarnoldy.fr>
2024-10-10 20:28:17 +0200

    Done exercices

--- Exercice10/index.js
+++ Exercice10/index.js
@@ -1,3 +1,7 @@
-//votre code ici
-
-export default printNumbers
+export default function printNumbers(n) {
+    let result = '';
+    for (let i = 1; i <= n; i++) {
+        result += i + ' ';
+    }
+    return result.trim(); // Retirer l'espace supplémentaire à la fin
+}
--- Exercice9/script.js
+++ Exercice9/script.js
@@ -1,3 +1,8 @@
-// votre code ici
+function checkAge(age) {
+    let _age = Number(age)
+    if (_age < 18) return 'Vous êtes mineur.';
+    if (_age >= 18 && _age < 65) return 'Vous êtes majeur.';
+    if (_age >= 65) return 'Vous êtes senior.';
+}
 
 export default checkAge;
